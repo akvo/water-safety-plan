ab7c0285c5efe977fd0f080d106fc3fffe656307
dedenbangkit <mail@dedenbangkit.com>
2021-07-31 01:59:05 +0700

    [#3] Modify transform command

--- doc/run.sh
+++ doc/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-
-docker run --rm -v "$(pwd)":/app "$(docker build -q .)"
+docker build -t wsp-transform .
+docker run --rm -v "$(pwd)":/app wsp-transform
+docker rmi wsp-transform
+rm -rf ../backend/data
 cp -r data ../backend/data
 rm -rf data
